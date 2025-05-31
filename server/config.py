df209e3ca542951048b7fadde5fe3ed9bb487eff
marvelfriction <marvelousezechi0@gmail.com>
2025-05-31 22:42:06 +0100

    chore: updated env config

--- server/config.py
+++ server/config.py
@@ -3,6 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str
     token_password_key: str
+    cloudinary_cloud_name: str
+    cloudinary_api_key: str
+    cloudinary_api_secret: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
--- server/routes/song.py
+++ server/routes/song.py
@@ -6,13 +6,14 @@ from middleware.auth_middleware import auth_middleware
 import cloudinary
 import uuid
 from models.song import Song
+from config import settings
 
 router = APIRouter()
 
 cloudinary.config( 
-    cloud_name = "diz5fdpl4", 
-    api_key = "688494593427676", 
-    api_secret = "S9uH1MpspGxA3333_eZi1K8SgbM",
+    cloud_name = settings.cloudinary_cloud_name,
+    api_key = settings.cloudinary_api_key, 
+    api_secret = settings.cloudinary_api_secret,
     secure=True
 )
 
@@ -25,20 +26,24 @@ def upload_song(song: UploadFile = File(...),
                 db: Session = Depends(get_db),
                 auth_dict=Depends(auth_middleware)):
     
-    song_id = str(uuid.uuid4())
-    song_res = cloudinary.uploader.upload(song.file, resource_type="auto", folder=f"songs/{song_id}")
-    thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type="image", folder=f"songs/{song_id}")
-    # store in db
-    new_song = Song(id=song_id,
-                    song_url= song_res["url"],
-                    thumbnail_url= thumbnail_res["url"],
-                    artist=artist,
-                    song_name=song_name,
-                    hex_code=hex_code)
-    db.add(new_song)
-    db.commit()
-    db.refresh(new_song)
-    return new_song
+    try:
+        song_id = str(uuid.uuid4())
+        song_res = cloudinary.uploader.upload(song.file, resource_type="auto", folder=f"songs/{song_id}")
+        thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type="image", folder=f"songs/{song_id}")
+        # store in db
+        new_song = Song(id=song_id,
+                        song_url= song_res["url"],
+                        thumbnail_url= thumbnail_res["url"],
+                        artist=artist,
+                        song_name=song_name,
+                        hex_code=hex_code)
+        db.add(new_song)
+        db.commit()
+        db.refresh(new_song)
+        return new_song
+    
+    except Exception as e:
+        raise HTTPException(400, f"Error uploading song: {str(e)}")
 
 @router.get("/list")
 def list_songs(db: Session = Depends(get_db),
